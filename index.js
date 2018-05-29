061bdc86d7f05acc8dec09d424dcc481ff004f82
Francisco <javiermartincuenca@gmail.com>
2018-05-29 03:29:30 +0200

    Add route and get route added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi=require('hapi');
 const mongooose=require('mongoose');
+const Painting=require('./models/Painting');
 
 //Add connection to database
 mongooose.connect('');
@@ -20,8 +21,29 @@ const init=async () => {
     method:'GET',
     path:'/',
     handler: function(request,reply){
-      return `<h1>My modern API</h1>`;
+        return `<h1>My modern API</h1>`;
+      }
+  },
+  {
+    method:'GET',
+    path:'/api/v1/paintings',
+    handler:(req,reply)=>{
+      return Painting.find();
     }
+  },{
+    method: 'POST',
+    path:'api/v1/paintings',
+    handler:(req,reply)=>{
+      const {name,url,techniques}=req.payload;
+      const painting=new Painting({
+        name,
+        url,
+        techniques
+      });
+
+      return painting.save();
+    }
+
   });
 
   await server.start();
